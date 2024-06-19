e0b62a28078aa8575c87935767556c558c95a07b
tangjijiangoodcloudscn <14147774+tangjijiangoodcloudscn@user.noreply.gitee.com>
2024-06-19 15:32:21 +0800

    first

---
+++ src/Net/Spider.php
@@ -0,0 +1,15 @@
+<?php
+namespace Zero\Dota\Net;
+
+class Spider
+{
+    public function __construct(){
+        return "__construct";
+    }
+    public function __toString()
+    {
+        // TODO: Implement __toString() method.
+
+        return 'this is a spider';
+    }
+}
